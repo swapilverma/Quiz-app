6427d2f267fe4f49f16f5bb648a68ee01c3be646
Swapil Verma <65918660+sv-coder-engg@users.noreply.github.com>
2021-05-25 21:10:40 +0530

    feat: added score and highest scorer

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+//IMPORTS
 var readlineSync = require("readline-sync");
 const chalk = require("chalk");
 
+//FUNCTION
+function play(ques,option,ans) {
+  console.log(ques);
+  var index = readlineSync.keyInSelect(option);
+
+  if (option[index] === ans ) {
+    console.log(chalk.bgGreen.blue.bold("Right!"));
+    score++;
+  }
+  else {
+    console.log(chalk.bgRedBright.black.bold("Wrong!"));
+    console.log(chalk.hex("#064E3B").bold("The correct answer was = "+ option[index]));
+  }
+  console.log("Score : "+ score);
+  if (score === 5){
+    console.log(chalk.hex("#064E3B").bold("\n\nWoW...You have now moved to level 2\n"))
+  }
+
+  if (score === 10){
+    console.log(chalk.hex("#064E3B").bold("Looks like we have got the biggest fan of FRIENDS here. \n Congrats, you have topped the chart"))
+  }
+  console.log("---------------------------------------");
+}
+
+//INPUTS
 name = readlineSync.question(chalk.hex("#064E3B").bold("Type your Name = "));
 console.log(chalk.hex("#F59E0B").bold("\nHI "+name+"!!\nLets play a quiz and see how much you know about FRIENDS \n"));
 
-var score = 0 ;
+//QUES-ANS
 var ques1 = ("How many sisters does Joey have?");
 var option1 = ["6","7","5","4"];
 
@@ -40,34 +66,37 @@ var option = [option1, option2, option3, option4, option5, option6, option7, opt
 
 var ans = ["7","Ross","3", "Janine","Ross", "Chandler", "Animals dressed as Humans", "Nose", "Maurice", "Chandler" ];
 
-function play(ques,option,ans) {
-  console.log(ques);
-  var index = readlineSync.keyInSelect(option);
-
-  if (option[index] === ans ) {
-    console.log(chalk.bgGreen.blue.bold("Right!"));
-    score++;
-  }
-
-  else {
-    console.log(chalk.bgRedBright.black.bold("Wrong!"));
-    console.log(chalk.hex("#064E3B").bold("The correct answer was = "+ option[index]));
-  }
-  console.log("Score : "+ score);
-  if (score === 5){
-    console.log(chalk.hex("#064E3B").bold("\n\nWoW...You have now moved to level 2\n"))
-  }
-
-  if (score === 10){
-    console.log(chalk.hex("#064E3B").bold("Looks like we have got the biggest fan of FRIENDS here. \n Congrats, you have topped the chart"))
-  }
-  console.log("----------------------------------");
-}
-
+//PROCESSING
+var score = 0 ;
 for (var i=0; i<ques.length; i++) {
     play(ques[i],option[i],ans[i]);
-  }
-  
-console.log(chalk.hex("#F59E0B").bold("\nYour total Score = "+score));
+}
 
-console.log(chalk.hex("#F59E0B").bold("Thanks for playing"))
+//OUTPUT
+console.log(chalk.hex("#F59E0B").bold("\nYour total Score = "+score));
+console.log(chalk.hex("#F59E0B").bold("Thanks for playing!!!"))
+
+//DATA FROM FREINDS 
+var data = {
+  Simphia: 3,
+  Sehaj : 7,
+  Aditya : 5,
+  Abhijeet :8 ,
+}
+
+console.log (chalk.hex("#064E3B").bold("\n\nScores collected\n"))
+
+//PROCESSING
+var highScorer, max = 0;
+for (const [key, value] of Object.entries(data)) {
+  console.log(chalk.hex("#F59E0B").bold(key+":"+value))
+  if (value > max) {
+    max = value
+    highScorer = key
+  } 
+}
+
+//OUTPUT
+console.log (chalk.hex("#064E3B").bold.bgWhite("\nHighest Scorer = "+ highScorer));
+
+//END OF CODE
